0a8e5dcfa2f2486b1128cf27228ae70451774295
SofiaPigino <sofipigino98@gmail.com>
2021-02-09 19:05:51 -0300

    Section 5

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -18,6 +19,37 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe(
       clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void {
+    const swalWithBootstrapButtons = swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+      title: 'Esta seguro?',
+      text: `Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.clienteService.delete(cliente.id).subscribe(response => {
+          this.clientes = this.clientes.filter(cli => cli !== cliente)
+          swalWithBootstrapButtons.fire(
+            'Cliente Eliminado!',
+            `Cliente ${cliente.nombre} eliminado con exito.`,
+            'success'
+          )
+        })
+      }
+    })
   }
 
 }
